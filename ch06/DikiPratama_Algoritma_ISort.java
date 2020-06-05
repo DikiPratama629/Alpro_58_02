5d56b3ef868a4cfbd216e0f1c4d5e35115b88bd3
DikiPratama629 <64294842+DikiPratama629@users.noreply.github.com>
2020-06-05 19:38:43 +0700

    Add files via upload

---
+++ ch06/DikiPratama_Algoritma_ISort.java
@@ -0,0 +1,9 @@
+
+package ch06;
+
+public class Algoritma_ISort {
+    public static void main(String[] args) {
+        int aar[] = {82, 12, 41, 38, 19, 26, 9, 48, 20, 55, 8, 32, 3};
+        
+    }
+}
---
+++ ch06/DikiPratama_Algoritma_SSort.java
@@ -0,0 +1,13 @@
+
+package ch06;
+
+public class Algoritma_SSort {
+    public static void main(String[] args)
+    { 
+        
+        int aar [] = {82, 12, 41, 38, 19, 26, 9, 48, 20, 55, 8, 32, 3};
+        ssort.sort(aar);
+        System.out.println("Data Array setelah di SSORT ");
+        ssort.printArray(aar);
+    }
+}
